215c9d420cd45a682b4d60ac1cf5e36e51c6e4c8
Alex Gleason <alex@alexgleason.me>
2021-11-23 19:49:59 -0600

    Add Hex metadata

--- mix.exs
+++ mix.exs
@@ -6,8 +6,33 @@ defmodule MastodonClient.MixProject do
       app: :mastodon_client,
       version: "0.1.0",
       elixir: "~> 1.11",
+      name: "mastodon_client",
+      description: "Lightweight HTTP client for Mastodon and Pleroma based on Tesla.",
+      source_url: "https://gitlab.com/soapbox-pub/mastodon_client",
+      package: package(),
+      docs: docs(),
+      elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
       deps: deps()
+    ]
+  end
+
+  def package do
+    [
+      licenses: ["MIT"],
+      links: %{"GitLab" => "https://gitlab.com/soapbox-pub/mastodon_client"}
+    ]
+  end
+
+  def docs do
+    [
+      main: "readme",
+      extras: [
+        "README.md",
+        "LICENSE.md"
+      ],
+      groups_for_modules: groups_for_modules(),
+      source_ref: "v0.1.0"
     ]
   end
 
@@ -18,6 +43,10 @@ defmodule MastodonClient.MixProject do
     ]
   end
 
+  # Specifies which paths to compile per environment.
+  defp elixirc_paths(:test), do: ["lib", "test/support"]
+  defp elixirc_paths(_), do: ["lib"]
+
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
@@ -26,5 +55,9 @@ defmodule MastodonClient.MixProject do
       {:jason, "~> 1.2", optional: true},
       {:ex_doc, ">= 0.24.2", only: :dev, runtime: false}
     ]
+  end
+
+  defp groups_for_modules do
+    []
   end
 end
